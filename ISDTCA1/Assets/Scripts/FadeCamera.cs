fd1c54a71985d64936f2c6d833b179882e22b56e
ZytronZyt <Zytron_ng@hotmail.com>
2021-12-19 20:22:59 +0800

    Labtask2 added on

---
+++ ISDTCA1/Assets/Scripts/FadeCamera.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+public class FadeCamera : MonoBehaviour
+{
+    #region Variables
+    //Private Variables
+
+    [SerializeField] private RectTransform _fadeScreenRectTransform;
+
+    [Header("Fade Settings")]
+    [SerializeField] [Range(0.1f, 3.0f)] private float _fadeInTime = 1.0f;
+    [SerializeField] [Range(0.1f, 3.0f)] private float _fadeOutTime = 1.0f;
+    #endregion
+
+    #region Unity Methods
+    // Start is called before the first frame update
+    protected void Start()
+    {
+        var seq = LeanTween.sequence();
+        seq.append(6f);
+        seq.append( () => {
+            FadeOutCam();
+        });
+        seq.append(5f);
+        seq.append( () => {
+            FadeInCam();
+        });
+    }
+    #endregion
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    #region Own Methods
+
+    public void FadeInCam(){
+        LeanTween.alpha(_fadeScreenRectTransform, 0f, _fadeInTime);
+    }
+
+    public void FadeOutCam(){
+        LeanTween.alpha(_fadeScreenRectTransform, 0f, _fadeOutTime);
+    }
+    #endregion
+}
